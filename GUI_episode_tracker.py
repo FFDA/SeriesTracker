c54df147a515f84e897d5629c7c9f870f579d8c2
FFDA <nebeklausk@gmail.com>
2019-03-03 20:20:39 +0200

    Made that episode tables in “Show Info” window and “Mark episode as watched” window show bottom line when those windows open. Implemented function that allows user to mark all episodes up to selected one as watched. This window displays all episodes that are in seasons (no in “Unknown” season) in separate 3x3 tables. Those tables a places in 3x3 grid too. Right now it is fully functional but need need make it more fix how it looks.

--- GUI_episode_tracker.py
+++ GUI_episode_tracker.py
@@ -603,6 +603,8 @@ class OpenMarkAsNotWatched(QWidget):
 		self.episodes_table.sql_select_shows = "SELECT * FROM %s" % self.IMDB_id
 		self.episodes_table.create_table()
 		
+		self.episodes_table.episode_table.scrollToBottom()
+		
 		self.create_buttons()
 		
 		self.layout.addWidget(self.button_box)
@@ -691,6 +693,8 @@ class OpenShowWindow(QWidget):
 
 		self.episodes_table.sql_select_shows = "SELECT * FROM %s" % self.IMDB_id
 		self.episodes_table.create_table()
+		
+		self.episodes_table.episode_table.scrollToBottom() # Scrolls table view to the bottom
 		
 		self.create_buttons()
 
@@ -805,7 +809,7 @@ class OpenShowWindow(QWidget):
 		mark_as_button_menu = QMenu()
 		mark_as_button_menu.addAction("episode as not watched", self.open_mark_episode_as_not_watched)
 		mark_as_button_menu.addAction("season as not watched", self.open_mark_season_as_not_watched)
-		mark_as_button_menu.addAction("up to episode as watched")
+		mark_as_button_menu.addAction("up to episode as watched", self.mark_up_to_episode_as_watched)
 		mark_as_button_menu.addAction("season as watched", self.open_mark_season_as_watched)
 				
 		# Creates button menu for "Update ..." button
@@ -858,15 +862,22 @@ class OpenShowWindow(QWidget):
 		self.open_mark_episode_as_not_watched_window.destroyed.connect(self.episodes_table.refill_episode_table)
 		
 	def open_mark_season_as_not_watched(self):
-		self.open_mark_season_as_not_watched_window = MarkSeasonAsNotWatched(self.IMDB_id, self.seasons, self.unknown_season)
+		self.open_mark_season_as_not_watched_window = MarkSeasonAsNotWatched(self.IMDB_id, self.seasons, self.unknown_season, self.title)
 		result = self.open_mark_season_as_not_watched_window.exec_()
 		
 		if result == QDialog.Accepted:
 			self.refill_episode_table()
 		
 	def open_mark_season_as_watched(self):
-		self.open_mark_season_as_watched_window = MarkSeasonAsWatched(self.IMDB_id, self.seasons, self.unknown_season)
+		self.open_mark_season_as_watched_window = MarkSeasonAsWatched(self.IMDB_id, self.seasons, self.unknown_season, self.title)
 		result = self.open_mark_season_as_watched_window.exec_()
+		
+		if result == QDialog.Accepted:
+			self.refill_episode_table()
+	
+	def mark_up_to_episode_as_watched(self):
+		self.open_mark_up_to_episode_as_watched = MarkUpToEpisodeAsWatched(self.IMDB_id, self.title, self.seasons, self.unknown_season)
+		result = self.open_mark_up_to_episode_as_watched.exec_()
 		
 		if result == QDialog.Accepted:
 			self.refill_episode_table()
@@ -876,12 +887,13 @@ class OpenShowWindow(QWidget):
 		
 class MarkSeasonAsNotWatched(QDialog):
 	
-	def __init__(self, IMDB_id, seasons, unknown_season):
-		super(QWidgit, self).__init__()
+	def __init__(self, IMDB_id, seasons, unknown_season, title):
+		super(QDialog, self).__init__()
 		self.IMDB_id = IMDB_id
 		self.seasons = seasons
 		self.unknown_season = unknown_season
-		self.window_title = "Choose season to mark as not watched"
+		self.title = title
+		self.window_title = "Choose season of %s to mark as watched" % self.title
 		self.message_text = "Mark season %s as not watched"
 		self.sql_season_mark = ""
 		self.initUI()
@@ -965,12 +977,13 @@ class MarkSeasonAsNotWatched(QDialog):
 
 class MarkSeasonAsWatched(MarkSeasonAsNotWatched):
 	
-	def __init__(self, IMDB_id, seasons, unknown_season):
+	def __init__(self, IMDB_id, seasons, unknown_season, title):
 		super(MarkSeasonAsNotWatched, self).__init__()
 		self.IMDB_id = IMDB_id
 		self.seasons = seasons
 		self.unknown_season = unknown_season
-		self.window_title = "Choose season to mark as watched"
+		self.title = title
+		self.window_title = "Choose season of %s to mark as watched" % self.title
 		self.message_text = "Mark season %s as watched"
 		self.sql_season_mark = ""
 		self.initUI()
@@ -979,6 +992,148 @@ class MarkSeasonAsWatched(MarkSeasonAsNotWatched):
 		if season != "":
 			self.message.setText(self.message_text % season)
 			self.sql_season_mark = "UPDATE %s SET episode_watched = 1 WHERE season = %s" % (self.IMDB_id, season)
+
+class MarkUpToEpisodeAsWatched(QDialog):
+	
+	def __init__(self, IMDB_id, title, seasons, unknown_season):
+		super(MarkUpToEpisodeAsWatched, self).__init__()
+		self.IMDB_id = IMDB_id
+		self.title = title
+		self.seasons = seasons
+		self.unknown_season = unknown_season
+		self.window_title = "Choose episode of %s" % self.title
+		self.message_text = "Select the episode and press confirm. All episodes up to selected one will be marked as watched."
+		self.sql_episode_mark = ""
+		self.initUI()
+		
+	def initUI(self):
+		self.setGeometry(400, 600, 800, 500)
+		self.setWindowTitle(self.window_title)
+		self.layout = QVBoxLayout()
+		
+		self.message = QLabel(self.message_text)
+		self.message.setWordWrap(True)
+		
+		self.create_confirmation_buttons()
+		
+		self.create_episode_tables()
+		
+		self.mark_message = QLabel("You haven't selected an episode")
+		
+		self.layout.addWidget(self.message)
+		self.layout.addWidget(self.scroll_area)
+		self.layout.addWidget(self.mark_message)
+		self.layout.addWidget(self.confirmation_button_box)
+		
+		self.setLayout(self.layout)
+		
+	def create_episode_tables(self):
+		
+		self.table_dict = {} # THis is a dictionary that holds every table in it.
+		
+		self.scroll_area = QScrollArea()
+		
+		self.scroll_area.setWidgetResizable(True)
+		self.episode_tables = QWidget()
+		self.episode_tables.layout = QGridLayout()
+		#self.episode_tables.layout.setSizeConstraint(QLayout.SetMinAndMaxSize)
+		
+		# Integers that hold values to make a 3x3 grid from tables.
+		widget_row_count = 0
+		widget_col_count = 0
+
+		for season in range(1, self.seasons + 1):
+			
+			self.table_box = QGroupBox()
+			self.table_box.layout = QVBoxLayout()
+			self.table_box.setTitle("%d Season" % season)
+			
+			self.table_dict["self.episode_table_widget_{0}".format(season)] = QTableWidget() # Creates dictory keys with name that starts "self.episode_table_widget_" and has season number at the end. Item for this dictionary item is a QTableWidget.
+			
+			current_table_widget = self.table_dict["self.episode_table_widget_{0}".format(season)] # Asings normalish name for the table widget
+			
+			current_table_widget.setColumnCount(3)
+			current_table_widget.setEditTriggers(QAbstractItemView.NoEditTriggers) # Makes widget not editable
+			
+			selected_episodes = QtSql.QSqlQuery("SELECT episode_seasonal_id FROM %s WHERE season = '%s'" % (self.IMDB_id, season))
+			
+			# Variables to make table as 3x3 grid.		
+			row_count = 0
+			col_count = 0
+			
+			current_table_widget.insertRow(row_count)
+			
+			while selected_episodes.next():
+						
+				if col_count == 3: # Resets tables column count every third coumn and adds an extra line to the table
+					col_count = 0
+					row_count += 1
+					current_table_widget.insertRow(row_count)
+				
+				current_table_widget.setItem(row_count, col_count, QTableWidgetItem(selected_episodes.value("episode_seasonal_id")))
+				
+				col_count += 1
+
+			current_table_widget.verticalHeader().setVisible(False)
+			current_table_widget.horizontalHeader().setVisible(False)
+			#episode_table_widget.setFixedSize(200, 300)
+			current_table_widget.resizeColumnsToContents()
+			
+			current_table_widget.cellClicked.connect(partial(self.make_sql_query, season))
+			
+			self.table_box.layout.addWidget(current_table_widget)
+			self.table_box.setLayout(self.table_box.layout)
+			
+			# Resest widget_col_count every third column
+			if widget_col_count == 3:
+				widget_col_count = 0
+				widget_row_count += 1
+			
+			widget_col_count += 1
+			
+			self.episode_tables.layout.addWidget(self.table_box, widget_row_count, widget_col_count) # Adds table to episode_tables grid
+			
+		self.episode_tables.setLayout(self.episode_tables.layout)
+		
+		self.scroll_area.setWidget(self.episode_tables)
+		
+	def make_sql_query(self, season, row, col):
+		# This function checks if user clicked on cell with data. If there is an episode_seasoal_id it sets/uptates sql query that will be used to update database and prints message to user.
+		# Otherwise it prints a message to user saying that he has to select an episode and sets sql_query string to empty one.
+		try:
+			episode_seasonal_id = self.table_dict["self.episode_table_widget_{0}".format(season)].item(row, col).text()
+			self.mark_message.setText("Episodes up to and including %s will be marked as watched" % episode_seasonal_id)
+			self.sql_episode_mark = "UPDATE %s SET episode_watched = 1 WHERE episode_seasonal_id <= '%s' AND episode != ''" % (self.IMDB_id, episode_seasonal_id)
+		except AttributeError:
+			self.mark_message.setText("You haven't selected an episode")
+			self.sql_episode_mark = ""
+			
+		
+	def create_confirmation_buttons(self):
+		# Creates button that will be displayed at the bottom of the Window to confirm or cancel action.
+		self.confirmation_button_box = QGroupBox()
+		self.confirmation_button_box.layout = QHBoxLayout()
+				
+		cancel = QPushButton("Cancel")
+		confirm = QPushButton("Mark as Watched")
+		
+		cancel.clicked.connect(self.reject) #This button rejects the action and closes window withour changing anything in database.
+		confirm.clicked.connect(self.mark_up_to_episode)
+		
+		self.confirmation_button_box.layout.addWidget(cancel)
+		self.confirmation_button_box.layout.addWidget(confirm)
+		
+		self.confirmation_button_box.setLayout(self.confirmation_button_box.layout)
+		
+	def mark_up_to_episode(self):
+		# Marks all episodes to as watched if query isn't empty.
+		# Otherwise prints a message to user.
+		if self.sql_episode_mark != "":
+			mark_query = QtSql.QSqlQuery(self.sql_episode_mark)
+			mark_query.exec_()
+			self.accept()
+		else:
+			self.mark_message.setText("Please choose an episode or click cancel")
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
